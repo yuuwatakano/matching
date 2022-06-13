70473f9f58a2745eac0443d8a225dfc2641adb40
yuuwatakano <y-takano@i-enter.co.jp>
2022-06-13 17:04:22 +0900

    6/13:アイコン設定機能追加・アカウントページへデータ受け渡し・リストにアイコン表示まで完了 6/13:リスト絞り込み完了 6/13:マッチング承認フィールド作成

--- app/src/main/java/jp/techacademy/yuuwa/takano/matching/AccountPageFragment.kt
+++ app/src/main/java/jp/techacademy/yuuwa/takano/matching/AccountPageFragment.kt
@@ -1,5 +1,6 @@
 package jp.techacademy.yuuwa.takano.matching
 
+import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.os.Bundle
@@ -10,10 +11,15 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.setFragmentResultListener
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import kotlinx.android.synthetic.main.activity_account_fragment.*
 import kotlinx.android.synthetic.main.fragment_account_page.*
 
 class AccountPageFragment : Fragment() {
 
+    private lateinit var mDataBaseReference: DatabaseReference
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -48,6 +54,19 @@ class AccountPageFragment : Fragment() {
         nameText.text = name
         genreText.text = genre
         skillText.text = skill
+
+        match_send_button.setOnClickListener { _ ->
+            val database = FirebaseDatabase.getInstance()
+            val favoriteref = database.getReference(FavoritePATH)
+            val user = FirebaseAuth.getInstance().currentUser
+            favoriteref.child(id.toString()).child(user!!.uid).setValue("favuser")
+        }
+        match_send_cansel_button.setOnClickListener { _ ->
+            val database = FirebaseDatabase.getInstance()
+            val favoriteref = database.getReference(FavoritePATH)
+            val user = FirebaseAuth.getInstance().currentUser
+            favoriteref.child(id.toString()).child(user!!.uid).setValue(null)
+        }
     }
 
     override fun onResume() {
