05078afdf3df6b4338fb808fc346e7c6f2a7c390
yuuwatakano <y-takano@i-enter.co.jp>
2022-06-17 10:07:03 +0900

    アカウント情報表示実装完了

---
+++ app/src/main/java/jp/techacademy/yuuwa/takano/matching/AccountChange.kt
@@ -0,0 +1,11 @@
+package jp.techacademy.yuuwa.takano.matching
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AccountChange : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_account_change)
+    }
+}
