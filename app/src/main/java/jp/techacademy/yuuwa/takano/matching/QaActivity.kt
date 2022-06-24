a3d82daeb0e3069d9359c5cf5ed9c9c15a041431
yuuwatakano <y-takano@i-enter.co.jp>
2022-06-24 16:14:15 +0900

    QA追加

--- app/src/main/java/jp/techacademy/yuuwa/takano/matching/QaActivity.kt
+++ app/src/main/java/jp/techacademy/yuuwa/takano/matching/QaActivity.kt
@@ -3,12 +3,15 @@ package jp.techacademy.yuuwa.takano.matching
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.activity_qa.*
 
 class QaActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_qa)
         window.statusBarColor = ContextCompat.getColor(this, R.color.transparent_status_bar)
-
+        back_buttom1.setOnClickListener {
+            finish()
+        }
     }
 }
