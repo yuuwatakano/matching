d58d105bde8775dbdeaaddb25f61c55d16d32492
yuuwatakano <y-takano@i-enter.co.jp>
2022-06-30 10:02:34 +0900

    コード整理完了

---
+++ app/src/main/java/jp/techacademy/yuuwa/takano/matching/AccountViewHolder.kt
@@ -0,0 +1,12 @@
+package jp.techacademy.yuuwa.takano.matching
+
+import android.view.View
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class AccountViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    val nameView: TextView = itemView.findViewById(R.id.nameTextView)
+    val addressView: TextView = itemView.findViewById(R.id.addressTextView)
+    val genreView: TextView = itemView.findViewById(R.id.genreTextView)
+    val skillView: TextView = itemView.findViewById(R.id.skillTextView)
+}
